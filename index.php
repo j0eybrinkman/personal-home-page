93446ea5fcad51a859a9e38cd46d9ae69068d07b
22halomedia <joey@22halomedia.com>
2025-03-08 15:00:22 -0700

    form

--- index.php
+++ index.php
@@ -3,3 +3,7 @@
 echo "<h1>Today's lucky number is " . rand() . "!</h1>";
 
 ?>
+
+<form method="post" action="#">
+    <label for="name">Your name: <input type="text" name="name" id="name" maxlength="255"></label><br>
+</form> 
